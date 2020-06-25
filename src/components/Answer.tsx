06cee69d7a0ba77687e40f0866c2608aa3c3016f
nagase123 <nagase0926@gmail.com>
2020-06-26 00:18:22 +0900

    デザイン修正

--- src/components/Answer.tsx
+++ src/components/Answer.tsx
@@ -1,23 +1,29 @@
 import React from 'react'
-// import { makeStyles, createStyles, Theme } from '@material-ui/core/styles'
+import { makeStyles, createStyles } from '@material-ui/core/styles'
 import Button from '@material-ui/core/Button'
 
-// const useStyles = makeStyles((theme: Theme) =>
-//   createStyles({
-//     root: {
-//       '& > *': {
-//         margin: theme.spacing(1),
-//       },
-//     },
-//   }),
-// )
+const useStyles = makeStyles(() =>
+  createStyles({
+    button: {
+      borderColor: '#FFB549',
+      color: '#FFB549',
+      fontWeight: 600,
+      marginBottom: '8px',
+      '&:hover': {
+        backgroundColor: '#FFB549',
+        color: '#fff',
+      },
+    },
+  }),
+)
 
 const Answer = (props: any) => {
-  //   const classes = useStyles()
+  const classes = useStyles()
 
   return (
     <Button
-      variant="contained"
+      className={classes.button}
+      variant="outlined"
       onClick={() => {
         props.select(props.content, props.nextId)
       }}
--- src/components/Chats.tsx
+++ src/components/Chats.tsx
@@ -1,17 +1,14 @@
 import React from 'react'
-import { createStyles, Theme, makeStyles } from '@material-ui/core/styles'
+import { createStyles, makeStyles } from '@material-ui/core/styles'
 import List from '@material-ui/core/List'
 import { Chat } from 'components/index'
 
-const useStyles = makeStyles((theme: Theme) =>
+const useStyles = makeStyles(() =>
   createStyles({
-    root: {
-      width: '100%',
-      maxWidth: '36ch',
-      backgroundColor: theme.palette.background.paper,
-    },
-    inline: {
-      display: 'inline',
+    chats: {
+      height: 400,
+      padding: 0,
+      overflow: 'auto',
     },
   }),
 )
@@ -20,7 +17,7 @@ const Chats = (props: any) => {
   const classes = useStyles()
 
   return (
-    <List className={classes.root}>
+    <List className={classes.chats}>
       {props.chats.map(
         (chat: { text: string; type: string }, index: number) => {
           return <Chat text={chat.text} type={chat.type} key={index.toString} />
